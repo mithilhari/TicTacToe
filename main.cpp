d256edff910f15182ab1f5fadf501d29e7fb27dc
Mithil Hari <mithilhari@yahoo.com>
2018-06-21 21:07:13 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdlib>
+using namespace std;
+
+bool gameOver() {
+
+    return false;
+}
+
+void updateBoard() {
+}
+
+int main()
+{
+    //while (!gameOver())
+    // A* algorithm	
+    return 0;
+
+}
