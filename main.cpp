438d67b21253f61b3a99d6e2582e4a8948dc5d07
Mithil Hari <mithilhari@yahoo.com>
2018-06-21 21:58:00 -0700

    testing priority queue

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <stdlib>
+#include <iostream>
+#include <queue>
 using namespace std;
 
 bool gameOver() {
@@ -9,10 +10,17 @@ bool gameOver() {
 void updateBoard() {
 }
 
-int main()
-{
+int main() {
     //while (!gameOver())
     // A* algorithm	
+    priority_queue<int> g;
+    g.push(10);
+    g.push(1);
+    g.push(2);
+    g.push(3);
+    while (!g.empty()) {
+        cout << "\t" << g.top();
+	g.pop();
+    }
     return 0;
-
 }
